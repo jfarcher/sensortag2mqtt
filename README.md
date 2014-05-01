92213769d99a7e57d03b08abda45ee3e891ecfcc
Jon Archer <jf_archer@yahoo.com>
2014-05-01 22:48:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ sensortag2mqtt
 ==============
 
 TI BLE Sensortag data to mqtt
+This python script takes data from a Texas instruments sensortag and publishes it to MQTT.
+
+The script takes the below work from Michael Saunby and extends it to incorporate output to MQTT,
+
+It currently takes 2 input forms, a mac address of the sensortag (see below) and a config file,
+the config file allows the mqtt broker details to be stored, a sample file exists to work from.
+
+
+Original work to output temperature data to console:
+
+ Michael Saunby. April 2013
+
+ Read temperature from the TMP006 sensor in the TI SensorTag
+ It's a BLE (Bluetooth low energy) device so using gatttool to
+ read and write values.
+
+ Usage.
+ sensortag_temp2mqtt.py BLUETOOTH_ADR
+
+ To find the address of your SensorTag run 'sudo hcitool lescan'
+ You'll need to press the side button to enable discovery.
+
+ Notes.
+ pexpect uses regular expression so characters that have special meaning
+ in regular expressions, e.g. [ and ] must be escaped with a backslash.
+
+
+
---
+++ sensortag_temp2mqtt.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+#See Readme for details
+
+
+import pexpect
+import sys
+import time
+import mosquitto
+import ConfigParser
+bluetooth_adr = sys.argv[1]
+
+config_file = "temp2mqtt.ini"
+
+if __name__ == "__main__":
+    try:
+        config = ConfigParser.ConfigParser()
+        config.readfp(open(config_file, "r"))
+    except IOError as e:
+        print e
+        sys.exit(1)
+
+    try:
+        host = config.get("MQTT", "host") 
+    except ConfigParser.NoOptionError:
+        host = os.getenv("MQTT_BROKER", "localhost")
+    try:
+        port = config.getint("MQTT", "port") 
+    except ConfigParser.NoOptionError:
+        port = int(os.getenv("MQTT_BROKER", "1883"))
+    try:
+        prefix = config.get("MQTT", "prefix") 
+    except ConfigParser.NoOptionError:
+        prefix = "temperature"
+
+
+
+def floatfromhex(h):
+    t = float.fromhex(h)
+    if t > float.fromhex('7FFF'):
+        t = -(float.fromhex('FFFF') - t)
+        pass
+    return t
+
+
+# This algorithm borrowed from 
+# http://processors.wiki.ti.com/index.php/SensorTag_User_Guide#Gatt_Server
+# which most likely took it from the datasheet.  I've not checked it, other
+# than noted that the temperature values I got seemed reasonable.
+#
+def calcTmpTarget(objT, ambT):
+    mqtt = mosquitto.Mosquitto()
+    mqtt.connect(host, port)
+    mqtt.loop_start()
+    topic = prefix + bluetooth_adr +"/celsius"
+    m_tmpAmb = ambT/128.0
+    Vobj2 = objT * 0.00000015625
+    Tdie2 = m_tmpAmb + 273.15
+    S0 = 6.4E-14            # Calibration factor
+    a1 = 1.75E-3
+    a2 = -1.678E-5
+    b0 = -2.94E-5
+    b1 = -5.7E-7
+    b2 = 4.63E-9
+    c2 = 13.4
+    Tref = 298.15
+    S = S0*(1+a1*(Tdie2 - Tref)+a2*pow((Tdie2 - Tref),2))
+    Vos = b0 + b1*(Tdie2 - Tref) + b2*pow((Tdie2 - Tref),2)
+    fObj = (Vobj2 - Vos) + c2*pow((Vobj2 - Vos),2)
+    tObj = pow(pow(Tdie2,4) + (fObj/S),.25)
+    tObj = (tObj - 273.15)
+    payload = "%.2f" % tObj
+    print topic, payload
+    mqtt.publish(topic, payload, qos=0, retain=False)
+    mqtt.loop_stop()
+    mqtt.disconnect()
+    
+
+
+
+tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive')
+tool.expect('.*\[LE\]>', timeout=600)
+print "Preparing to connect. You might need to press the side button..."
+tool.sendline('connect')
+# test for success of connect
+# tool.expect('\[CON\].*>')
+# Alternative test for success of connect
+tool.expect('Connection successful.*\[LE\]>')
+tool.sendline('char-write-cmd 0x29 01')
+tool.expect('\[LE\]>')
+while True:
+    time.sleep(1)
+    tool.sendline('char-read-hnd 0x25')
+    tool.expect('descriptor: .*') 
+    rval = tool.after.split()
+    objT = floatfromhex(rval[2] + rval[1])
+    ambT = floatfromhex(rval[4] + rval[3])
+    #print rval
+    calcTmpTarget(objT, ambT)
+
+
